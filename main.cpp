3901c6ac07813ce89df3c6b5796340a28f9a61ec
Brokkoly <brendankrull@gmail.com>
2015-10-14 22:00:17 -0500

    Added excuse for the subtract function not being functional

--- main.cpp
+++ main.cpp
@@ -101,7 +101,7 @@ bool add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
 bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len){
 	//Assuming that we are doing number 1 minus number 2
 	int c = c1 - c2;
-
+	//Currently waiting to recieve the divide function code, then the subtract function can be implemented.
 	/*
 
 	The code contained in this block is not workable code
